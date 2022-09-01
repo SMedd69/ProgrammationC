266176bcd8e003194972eeebcfa5415fdbe222ae
SMedd69 <97759843+SMedd69@users.noreply.github.com>
2022-09-01 22:38:35 +0200

    Ajout de commentaire

--- forTest.c
+++ forTest.c
@@ -2,9 +2,13 @@
 
 void main()
 {
-	int i;
-	for (i = 1; i < 42; i++)
+	int i; //Variable nombre entier = i
+	for (i = 1; i < 42; i++) // Fonction for = On initialise "i" à 1; on indique la condition -> si i(1) est inferieur à 42; alors i++ (on incrémente i de 1) 
 	{
-		printf("Yoo = %d \n", i);
+		printf("Yoo = %d \n", i); // On affiche : Yoo = i <==
 	}
 }
+
+// Conclusion tant que i est inferieur à 42 alors on ajoute +1 à i jusqu'a se qu'il arrive à 42 <--
+
+// la fonction for est utile et courte. 
